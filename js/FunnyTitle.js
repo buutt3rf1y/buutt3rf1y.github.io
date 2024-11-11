ea85e6c4c610d6717280ca8336853785def929e0
butterfly-up <1833884625@qq.com>
2024-11-12 00:43:14 +0800

    Site updated: 2024-11-12 00:43:13

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -9,7 +9,7 @@
      }
      else {
          $('[rel="icon"]').attr('href', "https://img.dkdun.cn/v1/2024/12/83e0272660805f52.png");
-         document.title = 'o(╥﹏╥)o别走qwq' + OriginTitle;
+         document.title = 'o(╥﹏╥)o别走qwq';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
