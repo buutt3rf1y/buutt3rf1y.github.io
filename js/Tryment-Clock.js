2c4c2d0f88bd6a62ac4bc5b050e9cc967dd1c8b6
butterfly-up <1833884625@qq.com>
2025-03-16 19:26:48 +0800

    Site updated: 2025-03-16 19:26:48

---
+++ js/Tryment-Clock.js
@@ -0,0 +1,1 @@
+hexo.extend.injector.register('body_begin', '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Tokisaki-Galaxy/TrymenT-ClocK/TrymenT-ClocK.min.css"><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/Tokisaki-Galaxy/TrymenT-ClocK/TrymenT-ClocK.min.js"></script>', 'default');
